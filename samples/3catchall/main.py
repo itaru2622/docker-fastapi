0e1d8dff4ba28411282512511070bce0b3816442
itaru2622 <itaru2622@gmail.com>
2024-07-12 17:16:46 +0900

    update: samples/3catchall/main.py to show more info for debug

--- samples/3catchall/main.py
+++ samples/3catchall/main.py
@@ -5,26 +5,27 @@ import json
 
 app = FastAPI()
 
-@app.get    ("/{full_path:path}")
-@app.put    ("/{full_path:path}")
-@app.post   ("/{full_path:path}")
-@app.delete ("/{full_path:path}")
-@app.options("/{full_path:path}")
-@app.patch  ("/{full_path:path}")
-@app.trace  ("/{full_path:path}")
+@app.get    ("{full_path:path}")
+@app.put    ("{full_path:path}")
+@app.post   ("{full_path:path}")
+@app.delete ("{full_path:path}")
+@app.options("{full_path:path}")
+@app.patch  ("{full_path:path}")
+@app.trace  ("{full_path:path}")
 async def catch_all(req: Request, full_path: str):
     body = await req.body() or '{}'
     body = json.loads(body)
 
 #   print( {"path" : full_path, "headers": req._headers, "query": req._query_params.items(),  "reqScope": dict(req.scope) })
-    rtn ={ "method": req.scope.get("method"), "url": req.url._url, "query": dict(req._query_params),  "body": body, "Authorization": req._headers.get('Authorization'), "host" : req._headers.get('host') }
-    print(rtn)
-    return rtn
+    input ={ "method": req.scope.get("method"), "url": req.url._url, "query": dict(req._query_params),  "body": body, "Authorization": req._headers.get('Authorization'), "host" : req._headers.get('host'), "headers": dict(req._headers) }
+    print(input)
+    return input
 
-@app.websocket("/{full_path:path}")
+@app.websocket("{full_path:path}")
 async def catch_websocket(ws:WebSocket, full_path: str):
     await ws.accept()
-    await ws.send_text(f'recieve connection at: {full_path}')
+    input = { "full_path": full_path, "query": dict(ws._query_params), "headers": dict(ws._headers) }
+    await ws.send_text(f'new websocket connection: {input}')
     while True:
         msg = await ws.receive_text()
         await ws.send_text(f'got: {msg}')
